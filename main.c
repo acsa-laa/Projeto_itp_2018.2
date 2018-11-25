17d64cfa9cb42b0529504be69b187f215f045d3b
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2018-11-25 17:12:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,24 +2,25 @@
 
 int main()
 {
+   setlocale(LC_ALL, "");
+
 	int escolha;
 
 	char continuidade;
 
 	printf("O que você quer fazer?\n");
-
 	
 	do{	
-		printf("-digite 1 para criar tabela\n-digite 2 para listar tabelas\n-digite 3 para criar nova linha na tabela\n-digite 4 para listar todos os dados de uma tabela\n-digite 5 para procurar um valor na tabela\n-digite 6 para apagar a linha de uma tabela\n-digite 7 para pagar uma tabela\n");
+		printf("-digite 1 para criar tabela\n-digite 2 para listar tabelas\n-digite 3 para criar nova linha na tabela\n-digite 4 para listar todos os dados de uma tabela\n-digite 5 para procurar um valor na tabela\n-digite 6 para apagar a linha de uma tabela\n-digite 7 para apagar uma tabela\n-digite 8 para criar uma nova coluna em uma tabela já existente\n-digite 9 para apagar uma coluna de uma tabela\n");
 		scanf("%d", &escolha);
 		switch (escolha)
 			{
    				case 1:
-     				criar_tabela();
+     					criar_tabela();
    					break;
 
    				case 2:
- 	    			listar_tabela();
+ 	    				listar_tabela();
    					break;
 
    				case 3:
@@ -33,8 +34,8 @@ int main()
    					break;
 
    				case 5:
-                  getchar();
-                  procurar_valor();
+                  			getchar();
+                 			procurar_valor();
    					break;
 
    				case 6:
@@ -47,7 +48,17 @@ int main()
    					apagar_tabela();
    					break;
 
-   				default:
+				case 8:
+					getchar();
+					criar_novaColuna();
+					break;
+
+			       case 9:
+				  	getchar();
+				  	apagar_coluna();
+				 	break;
+   				
+               			default:
    					printf("valor invalido\n");
    					break;
 		}
